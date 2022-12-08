5e042c9409ba5f9cfe068adda88862e3f828a971
jyotishka7139 <jyotishka.dasgupta@byjus.com>
2022-12-09 00:31:36 +0530

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("test one", () => {
+  test("renders learn react link", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+  test("renders learn react link", () => {
+    render(<App />);
+    const linkElement = screen.getByText("src/App.js");
+    expect(linkElement).toBeInTheDocument();
+  });
+  describe("test nested", () => {
+    test("renders learn react link", () => {
+      render(<App />);
+      const imgElement = screen.getByAltText("logo");
+      expect(imgElement).toBeInTheDocument();
+    });
+    test("renders learn react link", () => {
+      render(<App />);
+      const linkElement = screen.getByText("src/App.js");
+      expect(linkElement).toBeInTheDocument();
+    });
+  });
 });
